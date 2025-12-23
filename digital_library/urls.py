a20001429355644da8bb30406781f0371bcb04f2
Yakhob <jacobkurian2004@gmail.com>
2025-12-23 20:00:16 +0530

    Add files via upload

---
+++ digital_library/urls.py
@@ -0,0 +1,34 @@
+"""digital_library URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
+from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
+
+
+# Redirect the home page to the login page or dashboard
+def home_redirect(request):
+    return redirect('dashboard')  # Change to 'login' if needed
+
+urlpatterns = [
+    path('', home_redirect, name='home'),  # Add this line
+    path('admin/', admin.site.urls),
+    path('library/', include('library.urls')),
+   
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
